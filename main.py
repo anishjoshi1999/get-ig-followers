6df1bb8a2d87442be780fe71a47ec3f3982d4f10
anishjoshi1999 <anishjoshi1999@gmail.com>
2022-06-23 21:01:24 +0545

    comments added

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ from dotenv import load_dotenv
 load_dotenv()
 USERNAME = os.environ.get("USERNAME_INFO")
 PASSWORD = os.environ.get("PASSWORD")
-
+# Login
 cl = Client()
 cl.login(USERNAME, PASSWORD)
-
+# getting user_id from username for finding out the his/her followers
 user_id = cl.user_id_from_username("rojinakhatiwada37")
 print(user_id)
+# finding out followers via user_id
 followers = cl.user_followers(user_id)
 print(followers)
