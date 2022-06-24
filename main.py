0b86e4caf8f6e41564f49cc509ad6532ce8c82d5
anishjoshi1999 <anishjoshi1999@gmail.com>
2022-06-24 06:52:48 +0545

    obtain followers but only one user

--- main.py
+++ main.py
@@ -9,8 +9,11 @@ PASSWORD = os.environ.get("PASSWORD")
 cl = Client()
 cl.login(USERNAME, PASSWORD)
 # getting user_id from username for finding out the his/her followers
-user_id = cl.user_id_from_username("rojinakhatiwada37")
-print(user_id)
-# finding out followers via user_id
-followers = cl.user_followers(user_id)
-print(followers)
+names = ["rojinakhatiwada37", "sagartandan_", "saroz059"]
+for name in names:
+    user_id = cl.user_id_from_username(name)
+    # finding out followers via user_id
+    followers = cl.user_followers(user_id)
+    # total numbers of followers
+    total_followers = len(followers.keys())
+    print("{} has {} followers.".format(name, total_followers))
